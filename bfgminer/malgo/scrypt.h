d620af4c0295f80a9d6504ae5b67628190decbc2
Alexander1987 <meap09121@gmail.com>
2023-04-25 21:14:02 -0700

    Removed and Renamed Bfgminer for arm

--- bfgminer/malgo/scrypt.h
+++
@@ -1,6 +0,0 @@
-#ifndef SCRYPT_H
-#define SCRYPT_H
-
-extern void test_scrypt(void);
-
-#endif /* SCRYPT_H */
--- bfgminer/start_moonlander2.sh
+++
@@ -1,1 +0,0 @@
-./bfgminer --scrypt -o stratum+tcp://us.litecoinpool.org:3333 -u jstefanop.1 -p 1,d=128 -S ALL --set MLD:clock=600
